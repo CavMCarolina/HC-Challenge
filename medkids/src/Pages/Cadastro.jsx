20308e03ca47b3b735c1631147c6927580b9b551
unknown <eduardo.mazelli@gmail.com>
2024-05-14 09:08:20 -0300

    ajustes formularios e pagina do time

--- medkids/src/Pages/Cadastro.jsx
+++ medkids/src/Pages/Cadastro.jsx
@@ -5,63 +5,63 @@ export default function Cadastro() {
                 <div className="text-center py-3">
                     <h1 className="fs-1">Torne-se um Voluntário!</h1>
                     <h3 className="fs-5">Faça a diferença na vida de alguém.</h3>
-                    <p className="lead d-lg-none">O Hospital ds Clínicas está buscando voluntários dedicados e compassivos para se juntarem à nossa equipe. Oferecemos uma variedade de oportunidades para você doar seu tempo e talentos, e fazer um impacto real na vida de nossos pacientes.</p>
+                    <p className="lead d-lg">O Hospital das Clínicas está buscando voluntários dedicados e compassivos para se juntarem à nossa equipe. Oferecemos uma variedade de oportunidades para você doar seu tempo e talentos, e fazer um impacto real na vida de nossos pacientes.</p>
                 </div>
                 <hr className="my-4"/>
                 <h4>Dados Pessoais</h4>
-                <div class="row justify-content-center">
-                    <div class="col-lg-8">
-                        <form class="needs-validation" novalidate>
-                            <div class="row">
-                                <div class="col-md-6 mb-3"> 
-                                    <label for="nome" class="form-label">Nome</label>
-                                    <input type="text" id="nome" class="form-control" placeholder="Ex: João" required/>
+                <div className="row justify-content-center">
+                    <div className="col-lg-8">
+                        <form className="needs-validation">
+                            <div className="row">
+                                <div className="col-md-6 mb-3"> 
+                                    <label htmlFor="nome" className="form-label">Nome</label>
+                                    <input type="text" id="nome" className="form-control" placeholder="Ex: João" required/>
                                 </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="sobrenome" class="form-label">Sobrenome</label>
-                                    <input type="text" id="sobrenome" class="form-control" placeholder="Ex: Oliveira" required/>
+                                <div className="col-md-6 mb-3">
+                                    <label htmlFor="sobrenome" className="form-label">Sobrenome</label>
+                                    <input type="text" id="sobrenome" className="form-control" placeholder="Ex: Oliveira" required/>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-md-5 mb-3">
-                                    <label for="email">Email</label>
-                                    <input type="email" id="email" class="form-control" placeholder="nome@hotmail.com" required/>
+                            <div className="row">
+                                <div className="col-md-5 mb-4">
+                                    <label htmlFor="email">Email</label>
+                                    <input type="email" id="email" className="form-control" placeholder="nome@hotmail.com" required/>
                                 </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="tel">Telefone:</label>
-                                    <input type="tel" id="tel" class="form-control" placeholder="(DDD) 94002-8922" required/>
+                                <div className="col-md-4 mb-3">
+                                    <label htmlFor="tel">Telefone:</label>
+                                    <input type="tel" id="tel" className="form-control" placeholder="(DDD) 94002-8922" required/>
                                 </div>
-                                <div class="col-md-3 mb-3">
-                                    <label for="tel">Pref. de Contato:</label>
-                                    <select type="tel" id="tel" class="form-select" required>
+                                <div className="col-md-3 mb-3">
+                                    <label htmlFor="tel">Pref. de Contato:</label>
+                                    <select type="tel" id="tel" className="form-select" required>
                                         <option value="">Escolha...</option>
                                         <option value="tel">Telefone</option>
                                         <option value="email">Email</option>
                                     </select>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-md-3 mb-3">
-                                    <label for="cep" class="form-label">CEP</label>
-                                    <input type="text" id="cep" class="form-control" placeholder="Ex: 12345-678" required/>
+                            <div className="row">
+                                <div className="col-md-3 mb-3">
+                                    <label htmlFor="cep" className="form-label">CEP</label>
+                                    <input type="text" id="cep" className="form-control" placeholder="Ex: 12345-678" required/>
                                 </div>
-                                <div class="col-md-7 mb-3">
-                                    <label for="endereco" class="form-label">Endereço</label>
-                                    <input type="text" id="endereco" class="form-control" placeholder="Ex: Rua Abilio Soares" required/>
+                                <div className="col-md-7 mb-3">
+                                    <label htmlFor="endereco" className="form-label">Endereço</label>
+                                    <input type="text" id="endereco" className="form-control" placeholder="Ex: Rua Abilio Soares" required/>
                                 </div>
-                                <div class="col-md-2 mb-3">
-                                    <label for="numero" class="form-label">Número</label>
-                                    <input type="text" id="numero" class="form-control" placeholder="Ex: 52" required/>
+                                <div className="col-md-2 mb-3">
+                                    <label htmlFor="numero" className="form-label">Número</label>
+                                    <input type="text" id="numero" className="form-control" placeholder="Ex: 52" required/>
                                 </div>
                             </div>
-                            <div class="row">
-                                <div class="col-md-4 mb-3">
-                                    <label for="país" class="form-label">País</label>
-                                    <input type="text" id="país" class="form-control" placeholder="Ex: Brasil" required/>
+                            <div className="row">
+                                <div className="col-md-4 mb-3">
+                                    <label htmlFor="país" className="form-label">País</label>
+                                    <input type="text" id="país" className="form-control" placeholder="Ex: Brasil" required/>
                                 </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="Estado" class="form-label">Estado</label>
-                                    <select name="Estado" id="Estado" class="form-select" required>
+                                <div className="col-md-4 mb-3">
+                                    <label htmlFor="Estado" className="form-label">Estado</label>
+                                    <select name="Estado" id="Estado" className="form-select" required>
                                         <option value="">Escolha...</option>
                                         <option value="ac">Acre</option>
                                         <option value="al">Alagoas</option>
@@ -92,66 +92,66 @@ export default function Cadastro() {
                                         <option value="to">Tocantins</option>
                                     </select>
                                 </div>
-                                <div class="col-md-4 mb-3">
-                                    <label for="cidade" class="form-label">Cidade</label>
-                                    <input type="text" id="cidade" class="form-control" required />
+                                <div className="col-md-4 mb-3">
+                                    <label htmlFor="cidade" className="form-label">Cidade</label>
+                                    <input type="text" id="cidade" className="form-control" required />
                                 </div>
                             </div>
-                            <hr class="my-4"/>
+                            <hr className="my-4"/>
                             <h4>Área do voluntariado</h4>
-                            <div class="row">
-                                <div class="col-12 mb-3">
-                                    <label for="comentario" class="form-label">
+                            <div className="row">
+                                <div className="col-12 mb-3">
+                                    <label htmlFor="comentario" className="form-label">
                                     Por que você deseja trabalhar conosco?
-                                    <span class="text-muted">(Obrigatório)</span>
+                                    <span className="text-muted">(Obrigatório)</span>
                                     </label>
-                                    <textarea name="comentario" id="comentario" rows="3" class="form-control" maxlength="200" required ></textarea>
+                                    <textarea name="comentario" id="comentario" rows="3" className="form-control" maxLength="200" required ></textarea>
                                 </div>
-                                <div class="col-md-6 mb-3">
+                                <div className="col-md-6 mb-3">
                                     <p>Você toca algum instrumento?</p>
-                                    <input type="checkbox" class="form-check-input" value="positivo" id="positivo" required/>
-                                    <label for="positivo" class="me-5">Sim</label>
-                                    <input type="checkbox" class="form-check-input" value="negativo" id="negativo" required/>
-                                    <label for="negativo">Não</label>
+                                    <input type="checkbox" className="form-check-input" value="positivo" id="positivoI" required/>
+                                    <label htmlFor="positivoI" className="me-5">Sim</label>
+                                    <input type="checkbox" className="form-check-input" value="negativo" id="negativoI" required/>
+                                    <label htmlFor="negativoI">Não</label>
                                 </div>
-                                <div class="col-md-6 mb-3">
-                                    <label for="instrumentos">Se sim, quais?</label>
-                                    <textarea name="instrumentos" id="instrumentos" rows="1" class="form-control" maxlength="200" required ></textarea>
+                                <div className="col-md-6 mb-3">
+                                    <label htmlFor="instrumentos">Se sim, quais?</label>
+                                    <textarea name="instrumentos" id="instrumentos" rows="1" className="form-control" maxLength="200" required ></textarea>
                                 </div>
-                                <div class="col-md-4">
+                                <div className="col-md-4">
                                     <p>Gosta de contar piadas?</p>
-                                    <input type="checkbox" class="form-check-input" value="positivo" id="positivo" required/>
-                                    <label for="positivo" class="me-5">Sim</label>
-                                    <input type="checkbox" class="form-check-input" value="negativo" id="negativo" required/>
-                                    <label for="negativo">Não</label>
+                                    <input type="checkbox" className="form-check-input" value="positivo" id="positivoP" required/>
+                                    <label htmlFor="positivoP" className="me-5">Sim</label>
+                                    <input type="checkbox" className="form-check-input" value="negativo" id="negativoP" required/>
+                                    <label htmlFor="negativoP">Não</label>
                                 </div>
-                                <div class="col-md-4">
+                                <div className="col-md-4">
                                     <p>Aceitaria se fantasiar?</p>
-                                    <input type="checkbox" class="form-check-input" value="positivo" id="positivo" required/>
-                                    <label for="positivo" class="me-5">Sim</label>
-                                    <input type="checkbox" class="form-check-input" value="negativo" id="negativo" required/>
-                                    <label for="negativo">Não</label>
+                                    <input type="checkbox" className="form-check-input" value="positivo" id="positivoF" required/>
+                                    <label htmlFor="positivoF" className="me-5">Sim</label>
+                                    <input type="checkbox" className="form-check-input" value="negativo" id="negativoF" required/>
+                                    <label htmlFor="negativoF">Não</label>
                                 </div>
-                                <div class="col-md-4">
+                                <div className="col-md-4">
                                     <p>Aceitaria se vestir de palhaço?</p>
-                                    <input type="checkbox" class="form-check-input" value="positivo" id="positivo" required/>
-                                    <label for="positivo" class="me-5">Sim</label>
-                                    <input type="checkbox" class="form-check-input" value="negativo" id="negativo" required/>
-                                    <label for="negativo">Não</label>
+                                    <input type="checkbox" className="form-check-input" value="positivo" id="positivoPa" required/>
+                                    <label htmlFor="positivoPa" className="me-5">Sim</label>
+                                    <input type="checkbox" className="form-check-input" value="negativo" id="negativoPa" required/>
+                                    <label htmlFor="negativoPa">Não</label>
                                 </div>
                             </div>
-                            <hr class="my-4"/>
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" value="doacaoAlma" id="alma" required/>
-                                <label for="alma">Aceito os termos e politicas do Hospital das Clínicas<a href=""></a></label>
+                            <hr className="my-4"/>
+                            <div className="form-check">
+                                <input type="checkbox" className="form-check-input" value="doacaoAlma" id="alma" required/>
+                                <label htmlFor="alma">Aceito os termos e politicas do Hospital das Clínicas<a href=""></a></label>
                             </div>
                             <br/>
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" value="doacaoAlma" id="call" required/>
-                                <label for="call">Aceito ser contatado pelo Hospital das Clinicas<a href=""></a></label>
+                            <div className="form-check">
+                                <input type="checkbox" className="form-check-input" value="doacaoAlma" id="call" required/>
+                                <label htmlFor="call">Aceito ser contatado pelo Hospital das Clinicas<a href=""></a></label>
                             </div>
-                            <hr class="my-4"/>
-                            <button type="submit" class="btn btn-outline-primary w-100">Finalizar</button>
+                            <hr className="my-4"/>
+                            <button type="submit" className="btn btn-outline-primary w-100">Finalizar</button>
                         </form>
                     </div>
                 </div>
--- medkids/src/Pages/NossaEquipe.jsx
+++ medkids/src/Pages/NossaEquipe.jsx
@@ -1,7 +1,22 @@
+import data from '../../dados/integrantes.json'
+
 export default function NossaEquipe() {
     return (
         <>
-            <h1>Nossa Equipe</h1>
+            <h1>Nosso Time</h1>
+            <div className="lista-integrantes">
+                {
+                    data.map(
+                        (integrante, index) => (
+                            <div id="card" key={index}>
+                                <img src={integrante.foto}/>
+                                <h2>{integrante.nome}</h2>
+                                <p>Contato: {integrante.Contato}</p>
+                            </div>
+                        )
+                    )
+                    }
+            </div>
         </>
     )
 }
