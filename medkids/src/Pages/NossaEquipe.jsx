8786617671f416dcead1d76ced452af60045b2d2
eduardo <eduardo.mazelli@gmail.com>
2024-05-20 00:23:54 -0300

    Add API

--- medkids/src/Pages/NossaEquipe.jsx
+++ medkids/src/Pages/NossaEquipe.jsx
@@ -1,17 +1,24 @@
-import data from '../../dados/integrantes.json'
+import { useEffect,useState } from 'react'
 
 export default function NossaEquipe() {
+    const [integrantes, setIntegrantes] = useState([])
+
+    useEffect(() => {
+        fetch('https://664ab280a300e8795d42a416.mockapi.io/challenge/v1/time')
+            .then(response => response.json())
+            .then(data => setIntegrantes(data))
+        })
+      
     return (
         <>
             <h1>Nosso Time</h1>
             <div className="lista-integrantes">
-                {
-                    data.map(
-                        (integrante, index) => (
-                            <div id="card" key={index}>
-                                <img src={integrante.foto}/>
+                {integrantes.map(integrante => (
+                            <div id="card" key={integrante.id}>
+                                <img  src={integrante.foto}/>
                                 <h2>{integrante.nome}</h2>
-                                <p>Contato: {integrante.Contato}</p>
+                                <p>Contato: {integrante.email}</p>
+                                <a href={integrante.linkedin}><p>linkedIn</p></a>
                             </div>
                         )
                     )
