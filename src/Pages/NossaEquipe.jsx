99d1c86371113b3c22015d2dcc489ce300971257
eduardo <eduardo.mazelli@gmail.com>
2024-05-24 17:41:43 -0300

    ajustes finais

--- src/Pages/NossaEquipe.jsx
+++ src/Pages/NossaEquipe.jsx
@@ -1,8 +1,17 @@
 import email from "../assets/email.png";
 import linkedin from "../assets/linkedin.png";
-import data from "../../dados/integrantes.json";
+import { useEffect,useState } from 'react'
 
 export default function NossaEquipe() {
+
+  const [integrantes, setIntegrantes] = useState([])
+
+    useEffect(() => {
+        fetch('https://664ab280a300e8795d42a416.mockapi.io/challenge/v1/time')
+            .then(response => response.json())
+            .then(data => setIntegrantes(data))
+    })
+
   return (
     <>
       <main>
@@ -10,7 +19,7 @@ export default function NossaEquipe() {
         <div className="main_container">
           <h1>Nossa Equipe</h1>
           <div className="lista-integrantes">
-            {data.map((integrante, index) => (
+            {integrantes.map((integrante, index) => (
               <div id="card" key={index}>
                 <img src={integrante.foto} className="foto_integrante" />
                 <h2>{integrante.nome}</h2>
