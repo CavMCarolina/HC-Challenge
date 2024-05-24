fa604ec9fc1ca53c4311920e72d6445dffe2eba1
eduardo <eduardo.mazelli@gmail.com>
2024-05-24 17:26:05 -0300

    link no logo

--- src/Componentes/Header/Header.jsx
+++ src/Componentes/Header/Header.jsx
@@ -1,12 +1,18 @@
 import logo from "../../assets/logo_reduzido-MedKids.png";
 import Navbar from "../Navbar/Navbar";
+import home from "../../assets/home.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header id="header_container">
       <div id="cabecalho">
-        <img src={logo} alt="logo MedKids" />
-        <h1>MedKids</h1>
+        <Link to="/">
+          <img src={logo} alt="logo MedKids" />
+        </Link>
+        <Link to="/" className="titulo-cabecalho">
+          <h1>MedKids</h1>
+        </Link>
       </div>
       <Navbar />
     </header>
