ee128a0192b546914f8420475c8e277f6fe96299
CavMCarolina <RM552925@fiap.com.br>
2024-05-24 15:22:46 -0300

    reponsividade 404

--- src/Pages/PageNotFound.jsx
+++ src/Pages/PageNotFound.jsx
@@ -2,8 +2,15 @@ import bg from "../assets/404.png";
 
 export default function PageNotFound() {
   return (
-    <div className="page_not_found">
-      <img src={bg} />
-    </div>
+    <main>
+      <hr className="hr-normal" />
+      <div className="main_container">
+        <h1>404 - Página não encontrada</h1>
+        <h4>Infelizmente, não encontramos essa página. Tente voltar para a página anterior e abrir novamente</h4>
+        <div className="page_not_found">
+          <img src={bg} />
+        </div>
+      </div>
+    </main>
   );
 }
